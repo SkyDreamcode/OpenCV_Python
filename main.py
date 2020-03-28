6297109e1fccefa81e9f2660bac908013fb8724b
xuyw <3350554864@qq.com>
2020-03-28 18:26:20 +0800

    camera show

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/pyhton
 #from picamera_dir import picamera_src
 import picamera_dir
+import opencv_dir
 
 def main():
     #picamera_src.record_video_by_picamera()
@@ -11,7 +12,10 @@ def main():
     #picamera_dir.picamera_basic_recipes.capturing_to_a_stream()
     #picamera_dir.picamera_basic_recipes.capturing_to_a_PIL_image()
     #picamera_dir.picamera_basic_recipes.capturing_resize_image()
-    picamera_dir.picamera_basic_recipes.capturing_consistent_images()
+    #picamera_dir.picamera_basic_recipes.capturing_consistent_images()
+    #opencv_dir.opencv_src.get_pixel_value()
+    #opencv_dir.opencv_src.image_display_output()
+    opencv_dir.opencv_src.camera_video_show()
     print('main func is end');
 
 main()
